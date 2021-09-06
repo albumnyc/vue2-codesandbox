36ffe43adaab151ca8edab80259398b74694fdde
neiyichao03 <nieyichao03@kuaishou.com>
2021-09-06 17:04:17 +0800

    feat: 跨域

--- src/models/axios.ts
+++ src/models/axios.ts
@@ -10,16 +10,6 @@ axios.interceptors.response.use(
     res => {
         const { data = {}, config } = res;
 
-        // 正常 + 没开通服务号，内部处理，不用提示
-        if (+data.result === 1) {
-            return data;
-        }
-
-
-        if (+data.result === 109) {
-            return data;
-        }
-
         // 非1逻辑由业务内部通过try catch处理
         return data;
     },
